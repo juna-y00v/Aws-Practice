eda5748445548fa28c1f042f402892e5bea3fd83
Yjunha <junay00v@gmail.com>
2024-07-20 14:26:41 +0900

    init

---
+++ src/main/java/junay00v/aws_practice1/AwsPractice1Application.java
@@ -0,0 +1,13 @@
+package junay00v.aws_practice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsPractice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsPractice1Application.class, args);
+	}
+
+}
---
+++ src/test/java/junay00v/aws_practice1/AwsPractice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package junay00v.aws_practice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsPractice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
